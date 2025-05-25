91809f1ffca3728f90a895c27a20529f6197b985
arkonzz <arkonz@163.com>
2025-05-25 15:33:21 +0800

    限制程序单例运行

--- Program.cs
+++ Program.cs
@@ -2,17 +2,28 @@ namespace CountTimer
 {
     internal static class Program
     {
+        private static Mutex mutex = new Mutex(true, "{8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F}");
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            AntdUI.Config.ShowInWindow = true;
-            Application.Run(new MainForm());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                // To customize application configuration such as set high DPI settings or default font,
+                // see https://aka.ms/applicationconfiguration.
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                ApplicationConfiguration.Initialize();
+                AntdUI.Config.ShowInWindow = true;
+                Application.Run(new MainForm());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("程序已运行", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
